94b891f53383fc4eff509483662b502ae0b97140
goaliebaba <>
2021-05-29 03:45:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const adjOne = prompt("enter an adjective: ")
+const verbOne = prompt("enter a verb: ")
+const  username = prompt("enter a username or your real name: ")
+const realthing = "Hello my name is " + username +" "+ "today I'm fealing " + adjOne + "I like to " + " "+ verbOne + "."
+console.log(realthing)
